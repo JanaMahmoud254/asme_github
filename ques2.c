b17159115920d4693882d037d2b9229462e635ba
JanaMahmoud254 <jelsabahy@gmail.com>
2024-07-26 18:24:43 +0300

    ques2.c

--- ques2.c
+++ ques2.c
@@ -4,9 +4,10 @@
 
 int main(){
 
-    int num1, num2;
-    scanf("%d %d", &num1, &num2);
+    int num1, num2; //declaring two variables
+    scanf("%d %d", &num1, &num2); //using user input to get value of two variables
 
+    //output
     printf("%d + %d = %d\n", num1, num2, num1+num2);
     printf("%d - %d = %d\n", num1, num2, num1-num2);
     printf("%d * %d = %d\n", num1, num2, num1*num2);
